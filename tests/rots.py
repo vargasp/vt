899bba9d7590a531570d49403400721ff2177015
Phillip Vargas <vargasp@uchicago.edu>
2023-12-10 11:02:40 -0600

    Update rots.py

--- tests/rots.py
+++ tests/rots.py
@@ -12,6 +12,20 @@ import vt.comp_vision as cv
 from scipy.spatial import transform
 from scipy.ndimage import affine_transform
 
+def rankIdn(A, rank):
+    n,m = A.shape
+    
+    if rank > n or rank >m:
+        I = np.identity(rank)
+        n,m  = min(n,rank), min(m,rank)
+        I[:n,:m] = A[:n,:m]
+        return I
+    else:
+        return A[:rank,:rank]
+
+
+
+
 
 def transMat(coords,rank=None):
     coords = np.array(coords)
@@ -26,24 +40,35 @@ def transMat(coords,rank=None):
     return T
 
 
-def rotateMat(angs, center=None, seq='XYZ', extrinsic=True):
+def rotateMat(angs, center=None, seq='XYZ', extrinsic=True, rank=None):
+
+    #Converts angs to an np.array and calcualtes the number of angles
+    angs = np.array(angs)
+    n = angs.size
+
+    #
+    if rank==None and n==1:
+        rank = 2
+    elif rank==None:
+            
+
+    #If the angle is intrinsic lower the sequence 
     if not extrinsic:
         seq = str.lower(seq)
     
-    angs = np.array(angs)
-    n = angs.size
+    #Match the number of dimensions in the sequence to the number of angles
     seq = seq[(3-n):]
-        
+    
+    #Calcuatd a 3x3 rotation matrix (centered at the 0,0)
     R =  transform.Rotation.from_euler(seq, angs, degrees=True).as_matrix()
     
+    #Returns the R matrix or modifies it if rotation center is provided
     if center == None:
         return  R
     else:
-        RN = np.identity(4)
-        RN[:3,:3] = R
-        R = RN
-        
         T = transMat(center, rank=4)
+        R = rankIdn(R, 4)
+        
         print(T.shape)
         print(R.shape)
         return np.linalg.inv(T) @ R @ T
