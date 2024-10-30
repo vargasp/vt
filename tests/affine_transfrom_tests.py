a3a22cd301dab7d52b80c5e809b914ba5e99efb0
Phillip Vargas <vargasp@uchicago.edu>
2024-10-30 18:11:49 -0500

    affine testing

--- tests/affine_transfrom_tests.py
+++ tests/affine_transfrom_tests.py
@@ -12,15 +12,13 @@ import vir.affine_transforms as af
 import vir.sinogram as sg
 import vt
 
-from skimage.transform import radon
-from scipy.ndimage import affine_transform
+from scipy.ndimage import affine_transform, shift, rotate
+from skimage.transform import rotate as rotate2
 
 
 def affine3d(arr, mat):
     return affine_transform(arr,mat,order=1,cval=0.0)
 
-def angle_gif(images)
-    
 
 nX = 128
 nY = 128
@@ -41,21 +39,61 @@ test = af.coords_transform(phantom2d, coords)
 plt.imshow(test,origin='lower')
 
 T = af.transMat((16,16))
+T = np.linalg.inv(T)
 TC = (T @ coords)
 test = af.coords_transform(phantom2d, TC)
+test2 = shift(phantom2d, (16,16), order=1)
 plt.imshow(test,origin='lower')
+plt.show()
+plt.imshow(test2,origin='lower')
+plt.show()
 
-R = af.rotateMat(np.pi/4, center=np.array(phantom2d.shape)/2.0)
+
+R = af.rotateMat(np.pi/8, center=np.array(phantom2d.shape)/2.0 - 0.5)
+R = np.linalg.inv(R)
+RC = (R @ coords)
+test = af.coords_transform(phantom2d, RC)
+test2 = rotate(phantom2d,180/8, order=1,reshape=False)
+test3 = rotate2(phantom2d,180/8, order=1)
+
+plt.imshow(test,origin='lower')
+plt.show()
+plt.imshow(test2,origin='lower')
+plt.show()
+plt.imshow(test3,origin='lower')
+plt.show()
+
+
+R = af.rotateMat(np.pi/8, center=(32,32))
+R = np.linalg.inv(R)
+RC = (R @ coords)
+test = af.coords_transform(phantom2d, RC)
+test3 = rotate2(phantom2d,180/8, center=(32,32), order=1)
+
+plt.imshow(test,origin='lower')
+plt.show()
+plt.imshow(test3,origin='lower')
+plt.show()
+
+
+R = af.rotateMat(np.pi/8, center=np.array(phantom2d.shape)/2.0 - 0.5)
+R = af.rotateMat((0,.01,0), center=(64,0))
+
+R = np.linalg.inv(R)
 RC = (R @ coords)
 test = af.coords_transform(phantom2d, RC)
 plt.imshow(test,origin='lower')
+plt.show()
 
-R = af.rotateMat((0.,0.1,0.), center=(64,0))
+
+
+
+R = af.rotateMat((0.,0.0,0.), center=(64,0))
 print(R)
 RC = (R @ coords)
 test = af.coords_transform(phantom2d, RC)
 plt.imshow(test,origin='lower')
-
+plt.show()
 
 
 R = af.rotateMat((0.01,0,0.), center=(64,0))
